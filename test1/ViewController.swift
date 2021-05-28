82a20c3cf5ec0eb1684418d50e8ef0a47410b56b
SeolHui Gim <2750seolhee@naver.com>
2021-05-28 21:59:33 +0900

    Initial Commit

---
+++ test1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test1
+//
+//  Created by 설희pc on 2021/05/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
