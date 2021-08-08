e3dc36a452caccb8d97f3eaf98505ae1bfc87442
김설희 <2750seolhee@naver.com>
2021-08-08 20:42:30 +0900

    MyAlbum 프로젝트 생성

---
+++ MyAlbum/MyAlbum/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyAlbum
+//
+//  Created by 설희pc on 2021/08/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
