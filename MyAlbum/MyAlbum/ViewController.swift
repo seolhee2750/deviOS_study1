6f5c28be0ee4d5ad9c8f3195d3ae061e5958cdc5
김설희 <2750seolhee@naver.com>
2021-08-10 00:30:55 +0900

    뷰 구성

--- MyAlbum/MyAlbum/ViewController.swift
+++ MyAlbum/MyAlbum/ViewController.swift
@@ -13,15 +13,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     }
     
-    
-    @IBAction func hello(_ sender: Any) {
-        let alert = UIAlertController(title: "Hello", message: "It's my first App.", preferredStyle: .alert) // 알람 설정
-        let action = UIAlertAction(title: "OK", style: .default, handler: nil) // 알람에서 수행할 액션
-        alert.addAction(action) // 알람에 액션 추가
-        present(alert, animated: true, completion: nil) // 알람 띄우기
-    }
-    
-    
     @IBAction func info(_ sender: Any) {
         let alert = UIAlertController(title: "Info", message: "This is my practice button.", preferredStyle: .alert)
         let action = UIAlertAction(title: "OK", style: .default, handler: nil)
