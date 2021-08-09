1393defdd5c14f6a6f68b0fbabc685f70824fdea
김설희 <2750seolhee@naver.com>
2021-08-09 23:50:30 +0900

    create button

--- MyAlbum/MyAlbum/ViewController.swift
+++ MyAlbum/MyAlbum/ViewController.swift
@@ -11,9 +11,14 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func hello(_ sender: Any) {
+        let alert = UIAlertController(title: "Hello", message: "It's my first App.", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
 }
 
