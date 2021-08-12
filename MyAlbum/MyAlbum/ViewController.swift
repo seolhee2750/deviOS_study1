e94d54823e5ae5e62d375237c2a37cfaf4f4a67e
김설희 <2750seolhee@naver.com>
2021-08-12 21:25:04 +0900

    text 아웃렛 업데이트

--- MyAlbum/MyAlbum/ViewController.swift
+++ MyAlbum/MyAlbum/ViewController.swift
@@ -11,13 +11,17 @@ class ViewController: UIViewController {
     
     var currentValue = 0
     
+    
+    @IBOutlet weak var priceLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        priceLabel.text = "₩\(currentValue)"
     }
     
     @IBAction func info(_ sender: Any) {
         let message = "가격은 ₩\(currentValue) 입니다."
-        let alert = UIAlertController(title: "가격", message: message, preferredStyle: .alert)
+        let alert = UIAlertController(title: "Info", message: message, preferredStyle: .alert)
         let action = UIAlertAction(title: "OK", style: .default, handler: nil)
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
@@ -26,6 +30,7 @@ class ViewController: UIViewController {
         // Int로 형변환해준 이유는, 앞서 선언한 currentValue는 Int이고
         // arc4random_uniform은 Int32를 반환하기 때문에!
         currentValue = Int(arc4random_uniform(10000) + 1)
+        priceLabel.text = "₩\(currentValue)"
     }
 }
 
