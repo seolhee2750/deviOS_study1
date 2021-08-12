3296eee2849f6d7a7a3acfe7bbf3489b43b6906b
김설희 <2750seolhee@naver.com>
2021-08-12 21:36:39 +0900

    가격 업데이트 함수 생성

--- MyAlbum/MyAlbum/ViewController.swift
+++ MyAlbum/MyAlbum/ViewController.swift
@@ -11,21 +11,26 @@ class ViewController: UIViewController {
     
     var currentValue = 0
     
-    
+    // 가격 텍스트
     @IBOutlet weak var priceLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        priceLabel.text = "₩\(currentValue)"
+        refresh()
     }
     
+    // refresh 버튼 클릭 액션
     @IBAction func info(_ sender: Any) {
         let message = "가격은 ₩\(currentValue) 입니다."
         let alert = UIAlertController(title: "Info", message: message, preferredStyle: .alert)
         let action = UIAlertAction(title: "OK", style: .default, handler: nil)
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
-        
+        refresh()
+    }
+    
+    // 가격 업데이트 함수
+    func refresh() {
         // arc4random_uniform은 0부터 입력한 숫자까지 중 랜덤한 숫자를 출력
         // Int로 형변환해준 이유는, 앞서 선언한 currentValue는 Int이고
         // arc4random_uniform은 Int32를 반환하기 때문에!
